3a25b62e566f6ca64dc2410677bf1d588e8274e2
David <urzua.david@hotmail.com>
2021-04-19 21:23:45 -0500

    pattern

---
+++ example.py
@@ -0,0 +1,4 @@
+for i in range(6):
+    print("*" * (1 + i))
+for i in range(7, 0, -1):
+    print("*" * abs((1 - i)))
--- list_examples.py
+++ list_examples.py
@@ -1,2 +1,7 @@
 # Metodos de listas
-
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+# Start, Stop -1, Step
+print(thislist[2:5])
+# reverse string
+print(thislist[::-1])
