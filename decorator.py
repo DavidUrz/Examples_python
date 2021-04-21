97b4caf252e0110bf050fed455aa5c14bc684549
David <urzua.david@hotmail.com>
2021-04-20 23:30:41 -0500

    improve

--- decorator.py
+++ decorator.py
@@ -1,12 +1,22 @@
-def displaydecorator(fn):
-	def display_wrapper(message):
-		print('Output:')
-		fn(message)
-	return display_wrapper
-	
-@displaydecorator
-def display(message):
-	print("Inner Function")
-	print(message)
-	
-display("Hello")
+def func(f):
+	def wrapper(*args, **kwargs):
+		print("Started")
+		rv = f(*args, **kwargs)
+		print("Ended")
+		return rv
+	return wrapper
+
+
+@func
+def func2(x, y):
+	print(x)
+	return y
+
+
+@func
+def func3():
+	print("I am func3")
+
+
+x = func2(5, 6)
+print(x)
--- ex_json.py
+++ ex_json.py
@@ -9,3 +9,30 @@ print(json.dumps(31.76))
 print(json.dumps(True))
 print(json.dumps(False))
 print(json.dumps(None))
+
+# JSON to Python
+
+# some JSON:
+x = '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
+
+
+# Python to JSON
+
+# a Python object (dict):
+x = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
